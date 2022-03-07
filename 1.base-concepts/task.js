a59df9792217b56a5ed2d983488b1e72224963ca
Chebykina-Xenia <chebykina.xenia@gmail.com>
2022-03-07 17:18:43 +0300

    first commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,25 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = []; 
+  let root = 0; 
+  let discriminant = 0;
+  discriminant = Math.pow(b, 2) - 4 * a * c;
+  
+  if (discriminant < 0){
+	  arr = [];
+  }
+  
+  if (discriminant === 0){
+	  root = -b / (2 * a);
+	  arr[0] = root;
+  }
+  
+  if (discriminant > 0) {
+	  root = ((-b + Math.sqrt(discriminant)) / (2 * a));
+	  arr[0] = root;
+	  root = ((-b - Math.sqrt(discriminant)) / (2 * a));
+	  arr[1] = root;
+  }
+     
   return arr; // array
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
-}
