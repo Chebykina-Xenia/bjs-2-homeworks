73ed0eceb38cc56e152574914d074206aeed3107
Chebykina-Xenia <chebykina.xenia@gmail.com>
2022-03-10 16:53:26 +0300

    2 работа

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,65 @@
+//заполняем массив от -100 до 100
+let arr = [];
+for (let i = -100; i <= 100;){
+ arr.push(i++);
+}
+
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min, max, avg;
 
-  // Ваш код
+  min = Math.min(...arr);
+  max = Math.max(...arr);
+  
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  };
+  
+//  arg = parseFloat((sum / arr.length).toFixed(2));
+ arg = Number((sum / arr.length).toFixed(2));
 
-  return { min: min, max: max, avg: avg };
-}
+
+  return { min: min, max: max, arg: arg};
+};
+
 
 // Задание 2
+//сумма каждого массива в массиве
 function worker(arr) {
-  let sum;
+  let arrSum = 0;
+  if(Array.isArray(arr)) {
+       // let arrI = arr[i];
+       let sum = 0;
+       for(let j = 0; j < arr.length; j++){
+        sum += arr[j];
+      }
+      arrSum = sum;
+    } 
+    return  arrSum
+  }
 
-  // Ваш код
-
-  return sum;
-}
-
-function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
-  return max;
-}
+  function makeWork(arr,func){
+    let max = 0;
+    let x = [];
+    let sum = 0;
+    for(let i = 0; i < arr.length; ++i){
+      sum = func(arr[i]);
+      if (sum > max){
+        max = sum;
+      }
+    }
+    return max;
+  }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let max, min
+  let difference = 0;
+   if(Array.isArray(arr)) {
+     min = Math.min(...arr);
+     max = Math.max(...arr);
+     difference = Math.abs(max - min);          
+        }
+   return  difference
 }
