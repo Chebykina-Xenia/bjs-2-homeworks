0dfe49f1d7ad492f0de83714a34b66cd93640964
Chebykina-Xenia <chebykina.xenia@gmail.com>
2022-03-23 08:34:59 +0300

    ДЗ Асинхронность

--- 7.async/main.js
+++ 7.async/main.js
@@ -1,1 +1,16 @@
 // тут вы можете вызывать функции из task.js
+
+//создаём объект класса AlarmClock
+let phoneAlarm = new AlarmClock();
+
+phoneAlarm.addClock("08:28", () => console.log("Пора вставать!"), 1);
+
+phoneAlarm.addClock("08:29", () => console.log("Иди умываться!"), 2);
+
+phoneAlarm.removeClock(2);
+
+phoneAlarm.addClock("08:30", () => console.log("Вставай, а то проспишь!"), 3);
+
+phoneAlarm.printAlarms();
+
+phoneAlarm.start();
---
+++ 7.async/task.js
@@ -0,0 +1,81 @@
+class AlarmClock {
+  constructor() {
+    this.alarmCollection = [];   //массив звонков
+    this.timerId = null;
+  }
+
+  addClock(time, func, id) {
+    //если id не передан
+    if (isNaN(id)) {
+      throw new Error ("Параметр id не передан");
+    }
+
+    let arr = this.alarmCollection;
+    if (arr.some(obj => obj.id === id)){
+     console.error("Будильник с таким id уже существует")
+     return null;
+   }
+
+   let obj = {
+    time: time,
+    func: func,
+    id: id
+  }  
+
+  this.alarmCollection.push(obj);
+  return obj;
+}
+
+removeClock(id) {
+  let firstLength = this.alarmCollection.length;
+  let arr = this.alarmCollection;
+  let lasrArr = arr.filter(obj => obj.id != id);
+  let lastLength = lasrArr.length;
+  if (firstLength > lastLength){
+    this.alarmCollection = lasrArr;
+    return true;
+  }
+  return false;
+}
+
+getCurrentFormattedTime() {
+ let date = new Date();
+ let time = date.toTimeString().substr(0, 5);
+ return time;
+}
+
+  //функция проверки, принимает звонок
+  checkClock(phoneObj){
+    let sysdate = this.getCurrentFormattedTime();
+    let timePhone = phoneObj.time;
+    if (sysdate === timePhone){
+      return phoneObj.func;
+    }
+  }
+
+  //запускает все звонки
+  start (){ 
+    let arr = this.alarmCollection;
+    if (this.timerId === null){
+      setInterval (arr.forEach (obj => this.checkClock(obj)), 1000);
+   } 
+ }
+
+ stop(){
+  if (this.timerId != null){
+    clearInterval (this.start);
+    this.timerId === null;
+  }
+}
+
+printAlarms () {
+  let arr = this.alarmCollection;
+  arr.forEach (obj => console.log("Будильник № " + obj.id + " заведён на " + obj.time));
+}
+
+clearAlarms(){
+  this.stop();
+  this.alarmCollection = [];
+}
+}
+
